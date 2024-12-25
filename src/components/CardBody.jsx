f7910da6d73feeb63711136f7d26be2181facbe1
AlexVillalpando <42473462+AlexHVillalpando@users.noreply.github.com>
2024-12-25 05:14:18 -0600

    placeholder-modified

--- src/components/CardBody.jsx
+++ src/components/CardBody.jsx
@@ -11,43 +11,41 @@ function CardBody({ weather, handleSearch }) {
 
 	return (
 		<div className="card__body">
-				<form className="card__form" onSubmit={onSearch} name="search">
-					<button
-						className="card__form--btn"
-						type="submit"
-						onClick={onSearch}
-						name="input_search_btn"
-					>
-						<FaSearch />
-					</button>
+			<form className="card__form" onSubmit={onSearch} name="search">
+				<button
+					className="card__form--btn"
+					type="submit"
+					onClick={onSearch}
+					name="input_search_btn"
+				>
+					<FaSearch />
+				</button>
 
-					<div className="card__form--box-container">
-						<input
-							ref={inputRef}
-							className="card__form--box"
-							type="text"
-							placeholder="Enter city or country..."
-							name="input_search"
-						/>
-					</div>
-				</form>
-			<div className='bars'>
-			<div className="card__body--leftbar">
-				<div className="icon__container">
-					<img src={weather.icon} alt={weather.main} width={100} />
+				<div className="card__form--box-container">
+					<input
+						ref={inputRef}
+						className="card__form--box"
+						type="text"
+						placeholder="Enter city name..."
+						name="input_search"
+					/>
 				</div>
-				<h3 className="card__main">{`"${weather.main}"`}</h3>
-			</div>
+			</form>
+			<div className="bars">
+				<div className="card__body--leftbar">
+					<div className="icon__container">
+						<img src={weather.icon} alt={weather.main} width={100} />
+					</div>
+					<h3 className="card__main">{`"${weather.main}"`}</h3>
+				</div>
 
-			<div className="card__info">
-				<p className="card__wind-speed">Wind speed: {weather.wind} m/s</p>
-				<p className="card__clouds">Clouds: {weather.clouds}%</p>
-				<p className="card__pressure">
-					Barometric Pressure: {weather.pressure} hPa
-				</p>
-			</div>
-
-
+				<div className="card__info">
+					<p className="card__wind-speed">Wind speed: {weather.wind} m/s</p>
+					<p className="card__clouds">Clouds: {weather.clouds}%</p>
+					<p className="card__pressure">
+						Barometric Pressure: {weather.pressure} hPa
+					</p>
+				</div>
 			</div>
 		</div>
 	);
